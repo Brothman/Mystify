1f18ad08c6619c200589487cd81bf6d3c4c05c60
Ben Rothman <brothman7000@gmail.com>
2019-02-03 00:15:00 +0100

    Playing around with custom emmet snippets and todo highlighting

--- frontend/components/Home/index.jsx
+++ frontend/components/Home/index.jsx
@@ -1,3 +1,5 @@
+// @ts-check
+
 import React from 'react';
 import Sidebar from '../shared/sidebar/index.jsx';
 import Header from './header/index';
@@ -7,12 +9,16 @@ class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {  }
+        let x = 15;
+        x = 'hell';
+        
     }
     render() { 
         return ( 
             <div className="home">
                 <Sidebar />
                 <Header />
+                <Artist imageUrl="https://s3.us-east-2.amazonaws.com/mystify-images/NF_photo_2016.jpg" name="NF" />
             </div>
          );
     }
--- frontend/components/Shared/artist/index.jsx
+++ frontend/components/Shared/artist/index.jsx
@@ -1,10 +1,23 @@
-const Artist = ( { imageUrl, artist } ) => {
+import React from 'react';
+
+const Artist = ( { imageUrl, name } ) => {
     return ( 
         <div className="artist">
             <img src={imageUrl} alt="" className="artist__photo"/>
-            <p className="artist__name"> {artist} </p>
+            <p className="artist__name"> {name} </p>
         </div>
      );
 }
- 
-export default Artist;
+
+export default Artist; 
+
+
+//TODO say hello
+
+//BUG fix me
+
+//BUG df
+
+//FIXME please
+
+//TODO Hi
