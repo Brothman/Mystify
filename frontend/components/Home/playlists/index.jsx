81fdbd86cd4091bfa4769f5a5c3a86a5870c59b3
Benji Rothman <brothman7000@gmail.com>
2019-03-11 18:55:02 +0100

    Set up barebone folder structure to implement playlists

---
+++ frontend/components/Home/playlists/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Album from '../../Shared/album';
+
+export default class Playlist extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { };
+    }
+
+    createPlaylist() {
+
+    }
+
+    render() {
+        return (
+            <div className="home__playlist">
+                Hi
+            </div>
+        );
+    }
+};
