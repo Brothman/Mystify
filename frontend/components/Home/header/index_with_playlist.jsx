f79eaaf08f980e6840bebbf6e108d787e5a2d6c3
Benji Rothman <brothman7000@gmail.com>
2019-05-03 08:34:43 -0400

    Got rid of unimplemented Your Library and Playlist features

--- frontend/components/Home/header/index_with_playlist.jsx
+++ frontend/components/Home/header/index_with_playlist.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
-    return ( 
+    return (
         <div className="home__header">
             <NavLink activeClassName="home__nav-item--active" className="home__nav-item" to="/home/artists">Artists</NavLink>
             <NavLink activeClassName="home__nav-item--active" className="home__nav-item" to="/home/albums">Albums</NavLink>
             <NavLink activeClassName="home__nav-item--active" className="home__nav-item" to="/home/tracks">Tracks</NavLink>
-            <button className="home__new-playlist"> Inactive </button>
+            <NavLink activeClassName="home__nav-item--active" className="home__nav-item" to="/home/playlists">Playlists</NavLink>
+            <button className="home__new-playlist"> New Playlist </button>
         </div>
-     );
+    );
 }
- 
+
 export default Header;
--- frontend/components/Shared/sidebar/indexWithYourLibrary.jsx
+++ frontend/components/Shared/sidebar/indexWithYourLibrary.jsx
@@ -5,23 +5,23 @@ import { NavLink, Link } from 'react-router-dom';
 class Sidebar extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return ( 
+    render() {
+        return (
             <div className="sidebar">
                 <Link className="sidebar__nav-logo" to="/home/artists">
-                    <img src="https://s3.us-east-2.amazonaws.com/mystify-images/logo_mystify.png" alt="" className="sidebar__logo"/>                    
+                    <img src="https://s3.us-east-2.amazonaws.com/mystify-images/logo_mystify.png" alt="" className="sidebar__logo" />
                     <h1 className="sidebar__mystify">Mystify</h1>
                 </Link>
 
 
                 <NavLink activeStyle={{ color: 'rgba(255, 255, 255, 1)' }} className="sidebar__nav-item" to="/search">
-                        <div className="sidebar__svg-container">
-                            <svg viewBox="0 0 140 140" className="sidebar__svg-search"><circle cx="60" cy="60" r="45"></circle><path d="M 90 90 L 125 125"></path></svg>
-                        </div>
+                    <div className="sidebar__svg-container">
+                        <svg viewBox="0 0 140 140" className="sidebar__svg-search"><circle cx="60" cy="60" r="45"></circle><path d="M 90 90 L 125 125"></path></svg>
+                    </div>
 
-                        <p className="sidebar__nav-item-text"> Search </p>
+                    <p className="sidebar__nav-item-text"> Search </p>
                 </NavLink>
 
                 <NavLink activeStyle={{ color: 'rgba(255, 255, 255, 1)' }} className="sidebar__nav-item" to="/home/">
@@ -30,9 +30,16 @@ class Sidebar extends React.Component {
                     </div>
                     <p className="sidebar__nav-item-text"> Home </p>
                 </NavLink>
+
+                <NavLink activeStyle={{ color: 'rgba(255, 255, 255, 1)' }} className="sidebar__nav-item" to="/library">
+                    <div className="sidebar__svg-container">
+                        <svg viewBox="0 0 140 140" className="sidebar__svg-library"><path d="M 10 20 L 10 120"></path><path d="M 40 20 L 40 120"></path><path d="M 70 20 L 115 120"></path></svg>
+                    </div>
+                    <p className="sidebar__nav-item-text"> Your Library </p>
+                </NavLink>
             </div>
-         );
+        );
     }
 }
- 
+
 export default Sidebar;
